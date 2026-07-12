ee273d9f7ec6dff2b1e212c14fbe42ea2d25fa75
Dinurang <dinura1ginige@gmail.com>
2026-07-12 17:49:43 +0530

    initial file/folder strucuture

---
+++ backend/document-processing-pipeline/pdf-manager/upload.py
@@ -0,0 +1,3 @@
+# ingestion of pdf file/s into the system
+
+# passing to validator for validation
---
+++ backend/document-processing-pipeline/pdf-manager/validation.py
@@ -0,0 +1,9 @@
+# validation of pdf file/s
+
+
+
+# If the pdf file/s is/are valid, then it will be passed to the next step in the pipeline for further processing.
+
+
+
+# else, the pdf file/s will be rejected and an error message will be returned to the user.
